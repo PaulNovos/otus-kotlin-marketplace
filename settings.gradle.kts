fab0e69cac5d0190524f396f652053bd8588e026
NOVOSELOV Pavel <pavel.novoselov@raiffeisen.ru>
2023-01-29 15:11:20 +0500

    initial settings

---
+++ settings.gradle.kts
@@ -0,0 +1,12 @@
+
+rootProject.name = "ok-marketplace-202212"
+
+pluginManagement {
+    val kotlinVersion: String by settings
+
+    plugins {
+        kotlin("jvm") version kotlinVersion
+    }
+
+}
+
